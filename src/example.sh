150fefd201c8d92426c37d9e08bdc830f21f9d9d
Tyrone Chen <chen.tyrone91@gmail.com>
2020-04-22 21:44:35 +1000

    Updated example script to match updates to command line parsing

--- src/example.sh
+++ src/example.sh
@@ -1,15 +1,19 @@
-#!/bin/bash
-# example usage of code
-Rscript diablo_train.R ../data/classes_diablo.txt \
-	--classes_secondary ../data/pch.txt \
-	-d ../data/diablo_proteome.txt ../data/diablo_translatome.txt \
-	--rdata RData.RData \
-	-i 24 \
-	-p Rplots.pdf \
-	--dropna_classes TRUE \
-	--pcomp 10 \
-	--plsdacomp 10 \
-	--splsdacomp 5 \
-	--mdist mahalanobis \
-	--ncpus 6 \
-	--splsda_keepx 10 50 100 250
+Rscript diablo_train.R \
+   ../data/classes_diablo.txt  \
+   --args Rscript.sh \
+   --classes_secondary ../data/pch.txt \
+   --data ../data/diablo_proteome.txt ../data/diablo_translatome.txt \
+   --dropna_classes TRUE \
+   --dropna_prop 0 \
+   --ncpus 6 \
+   --dcomp 0 \
+   --icomp 5 \
+   --rdata RData.RData \
+   --plot Rplots.pdf \
+   --outfile_dir test \
+   --pcomp 10 \
+   --plsdacomp 4 \
+   --splsdacomp 4 \
+   --splsda_keepx 10,50,100,250 \
+   --mdist mahalanobis \
+   --contrib min
