773e8370336d2ebe8e32706420e899f52a5cd915
Tyrone Chen <chen.tyrone91@gmail.com>
2021-02-25 19:27:13 +0800

    Update R package with documentation and data

---
+++ multiomics/R/two_omics_data.R
@@ -0,0 +1,21 @@
+#' COVID-19 patient study
+#'
+#' Data from a COVID-19 patient study. Contains 100 anonymised patients.
+#' Patients are divided into two categories, less severe and more severe.
+#' In this subset, two omics datasets are featured - lipidome and metabolome.
+#' The original dataset contains four omics datasets.
+#'
+#' @docType data
+#'
+#' @usage data(two_omics)
+#'
+#' @format A list of two named dataframes containing two omics datasets. Also contains class information.
+#'
+#' @keywords datasets
+#'
+#' @references Overmyer, K.A., Shishkova, E., Miller, I.J., Balnis, J., Bernstein, M.N., Peters-Clarke, T.M., Meyer, J.G., Quan, Q., Muehlbauer, L.K., Trujillo, E.A. and He, Y., 2020. Large-scale multi-omic analysis of COVID-19 severity. Cell systems.
+#' (\href{https://www.ncbi.nlm.nih.gov/pubmed/32743614}{PubMed})
+#'
+#' @examples
+#' data(two_omics)
+"two_omics"
